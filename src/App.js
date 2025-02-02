633b0c553d27f6692205912b4ed1768bd6675ee0
RelentlessCore <dardankabashi101@gmail.com>
2025-02-02 23:21:04 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
-import Landing from './components/Landing'
-import Highlights from "./components/Highlights"; 
-import Featured from "./components/Featured";
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
+import { React, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([book]);
+    console.log(cart);
+  }
+
   return (
     <Router>
       <div className="App">
+        <Route />
         <Nav />
-        <Landing />
-        <Highlights />
-        <Featured />
-        <Discounted />
-        <Explore />
-        <Footer /> 
-      </div>  
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => <BookInfo books={books} addToCart={addToCart} />}
+        />
+        <Route path="/cart/" render={() => <Cart books={books} />} />
+        <Footer />
+      </div>
     </Router>
   );
 }
 
-export default App; 
+export default App;
 
-// 1:00:00
+// 1:40:10
--- src/pages/BookInfo.js
+++
@@ -1,5 +0,0 @@
-function BookInfo() {
-  return <h1>Book Info Page</h1>;
-}
-
-export default BookInfo;
