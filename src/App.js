b2fe4f8ffcb127cdab3bb4728914834571d6718d
Relentless <dardankabashi101@gmail.com>
2025-02-05 12:23:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import Books from "./pages/Books";
+import Books from "./pages/Books"; 
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo"; 
 import Cart from "./pages/Cart";
