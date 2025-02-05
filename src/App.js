83919441a7f69a93273013ae2455516edd07200c
Relentless <dardankabashi101@gmail.com>
2025-02-05 12:23:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
  
   return (
     <Router>
-      <div className="App">
+      <div className="App"> 
         <Route /> 
         <Nav />
         <Route path="/" exact component={Home} />
