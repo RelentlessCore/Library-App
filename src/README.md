1b87e1158e5e4ae36f265e36066dad9808b0b515
RelentlessCore <dardankabashi101@gmail.com>
2025-02-08 01:55:17 +0100

    commit

--- src/README.md
+++ src/README.md
@@ -77,3 +77,5 @@ function App() {
 }
 
 export default App;
+
+// 2:42:08
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -17,18 +16,6 @@ export default function BookInfo({ books, addToCart, cart }) {
   function bookExistsOnCart() {
     return cart.find((book) => +book.id === +id);
   }
-=======
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
-import Rating from '../components/ui/Rating';
-import Price from '../components/ui/Price';
-import Book from '../components/ui/Book';
-
-const BookInfo = ({ books, addToCart }) => {
-  const { id } = useParams();
-  const book = books.find(book => +book.id === +id);
->>>>>>> b2fe4f8ffcb127cdab3bb4728914834571d6718d
 
   return (
     <div id="books__body">
@@ -40,7 +27,6 @@ const BookInfo = ({ books, addToCart }) => {
                 <FontAwesomeIcon icon="arrow-left" />
               </Link>
               <Link to="/books" className="book__link">
-<<<<<<< HEAD
                 <h2 className="book__selected--title--top">Books</h2>
               </Link>
             </div>
@@ -83,40 +69,6 @@ const BookInfo = ({ books, addToCart }) => {
                     Add to cart
                   </button>
                 )}
-=======
-                <h2 className='book__selected--title--top'>Books</h2>
-              </Link>
-            </div>
-            <div className="book__selected">
-              <figure className='book__selected--figure'>
-                <img
-                  src={book.url}
-                  alt=""
-                  className='book__selected-img' />
-              </figure>
-              <div className="book__selected--description">
-                <h2 className="book__selected--title">
-                  {book.title}
-                </h2>
-                <Rating rating={book.rating} />
-                <div className="book__selected--price">
-                  <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
-                </div>
-                <div className="book__summary">
-                  <h3 className="book__summary--title">
-                    Summary
-                  </h3>
-                  <p className="book__summary--para">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam porro eum quod obcaecati architecto nisi distinctio inventore, nesciunt consequuntur accusantium itaque unde at modi nihil labore laborum officiis dolorum illum.
-                  </p>
-                  <p className="book__summary--para">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam porro eum quod obcaecati architecto nisi distinctio inventore, nesciunt consequuntur accusantium itaque unde at modi nihil labore laborum officiis dolorum illum.
-                  </p>
-                </div>
-                <button className="btn" onClick={() => addToCart(book)}>
-                  Add to cart
-                </button>
->>>>>>> b2fe4f8ffcb127cdab3bb4728914834571d6718d
               </div>
             </div>
           </div>
@@ -125,35 +77,17 @@ const BookInfo = ({ books, addToCart }) => {
         <div className="books__container">
           <div className="row">
             <div className="book__selected--top">
-<<<<<<< HEAD
               <h2 className="book__selected--title--top">Recommended Books</h2>
-=======
-              <h2 className="book__selected--title--top">
-                Recommended Books
-              </h2>
->>>>>>> b2fe4f8ffcb127cdab3bb4728914834571d6718d
             </div>
             <div className="books">
               {books
                 .filter((book) => book.rating === 5 && +book.id !== +id)
-<<<<<<< HEAD
                 .map((book) => <Book book={book} key={book.id} />)
                 .splice(0, 4)}
-=======
-                .slice(0, 4)
-                .map(book => <Book book={book} key={book.id} />)
-              }
->>>>>>> b2fe4f8ffcb127cdab3bb4728914834571d6718d
             </div>
           </div>
         </div>
       </main>
     </div>
   );
-<<<<<<< HEAD
 } 
-=======
-};
-
-export default BookInfo;
->>>>>>> b2fe4f8ffcb127cdab3bb4728914834571d6718d
